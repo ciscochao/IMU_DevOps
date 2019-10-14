cddbed79071241f1531a000df412504bf10e31e8
ciscochao <ciscochao@hotmail.com>
2019-10-14 18:17:11 +0800

    update test

--- assets/models.py
+++ assets/models.py
@@ -34,11 +34,12 @@ class Assets(models.Model):
     asset_tags = models.ManyToManyField('TagAssets', blank=True, verbose_name='资产标签')
     asset_admin = models.ForeignKey('users.UserProfile', blank=True, related_name='asset_admin',
                                     verbose_name='资产管理员', on_delete=models.PROTECT)
-    asset_idc = models.ForeignKey('IDCAssets', related_name='asset_idc', on_delete=models.PROTECT, null=True, blank=True,
-                                  verbose_name='所在机房')
-    asset_cabinet = models.ForeignKey('CabinetAssets', related_name='asset_cabinet', null=True, blank=True,
-                                      verbose_name='所在机柜')
-    asset_contract = models.ForeignKey('ContractAssets', null=True, blank=True, on_delete=models.SET_NULL, verbose_name='合同')
+    asset_idc = models.ForeignKey('IDCAssets', related_name='assets_idc', on_delete=models.PROTECT, null=True,
+                                  blank=True, verbose_name='所在机房')
+    asset_cabinet = models.ForeignKey('CabinetAssets', related_name='assets_cabinet', null=True, blank=True,
+                                      verbose_name='所在机柜', on_delete=models.PROTECT)
+    asset_contract = models.ForeignKey('ContractAssets', null=True, blank=True, on_delete=models.SET_NULL,
+                                       verbose_name='合同')
     asset_purchase_day = models.DateField(null=True, blank=True, verbose_name='购买日期')
     asset_expire_day = models.DateField(null=True, blank=True, verbose_name='过保日期')
     asset_price = models.CharField(max_length=100, null=True, blank=True, verbose_name='价格(RMB)')
@@ -60,6 +61,7 @@ class Assets(models.Model):
 
 class ServerAssets(models.Model):
     """服务器资产表"""
+
     server_type_choice = (
         (0, '塔式服务器'),
         (1, '刀片服务器'),
@@ -100,6 +102,7 @@ class ServerAssets(models.Model):
 
 class NetworkAssets(models.Model):
     """网络设备资产表"""
+
     network_type_choice = (
         (0, '路由器'),
         (1, '交换机'),
@@ -127,6 +130,7 @@ class NetworkAssets(models.Model):
 
 class SecurityAssets(models.Model):
     """安全设备资产表"""
+
     security_type_choice = (
         (0, 'FireWall'),
         (1, 'IPS'),
@@ -158,6 +162,7 @@ class SecurityAssets(models.Model):
 
 class StorageAssets(models.Model):
     """存储设备资源表"""
+
     storage_type_choice = (
         (0, '磁盘阵列'),
         (1, '网络存储'),
@@ -183,6 +188,7 @@ class StorageAssets(models.Model):
 
 class SoftAssets(models.Model):
     """软件类资产表"""
+
     soft_type_choice = (
         (0, '操作系统'),
         (1, '数据库授权'),
@@ -191,7 +197,7 @@ class SoftAssets(models.Model):
         (4, '业务软件'),
         (5, '其他'),
     )
-    assets = models.OneToOneField('Assets', on_delete=models.CASCADE)
+    assets = models.OneToOneField('Assets', related_name='soft_assets', on_delete=models.CASCADE)
     soft_type = models.SmallIntegerField(choices=soft_type_choice, default=5, verbose_name='软件资产类型')
     soft_license_num = models.IntegerField(max_length=100, default=1, verbose_name='授权数量')
     soft_version = models.CharField(max_length=100, blank=True, null=True, help_text="例如：Cent 5.2.9-2kali1",
@@ -208,6 +214,7 @@ class SoftAssets(models.Model):
 
 class OfficeAssets(models.Model):
     """办公相关设备资产表"""
+
     office_type_choice = (
         (0, '台式电脑'),
         (1, '笔记本'),
@@ -218,7 +225,7 @@ class OfficeAssets(models.Model):
         (6, '电话'),
         (7, '其他'),
     )
-    assets = models.OneToOneField('Assets', on_delete=models.CASCADE)
+    assets = models.OneToOneField('Assets', related_name='office_assets', on_delete=models.CASCADE)
     office_type = models.CharField(choices=office_type_choice, default=7, verbose_name='办公设备类型')
     office_sn = models.CharField(max_length=128, unique=True, verbose_name='设备序列号')
     office_user = models.CharField(max_length=100, unique=True, editable=True, verbose_name='使用人')
@@ -234,6 +241,7 @@ class OfficeAssets(models.Model):
 
 class ManufacturerAssets(models.Model):
     """厂商信息表"""
+
     manufacturer_name = models.CharField(max_length=100, unique=True, verbose_name='厂商名称')
     manufacturer_contacts = models.CharField(max_length=100, verbose_name='厂商联系人')
     manufacturer_phone = models.CharField(max_length=11, null=True, unique=True, verbose_name="联系人电话")
@@ -251,6 +259,7 @@ class ManufacturerAssets(models.Model):
 
 class BusinessAssets(models.Model):
     """业务线"""
+
     business_name = models.CharField(max_length=100, unique=True, verbose_name='业务线名称')
     business_charge = models.ForeignKey('users.UserProfile', blank=True, verbose_name='业务负责人',
                                         on_delete=models.PROTECT)
@@ -267,6 +276,7 @@ class BusinessAssets(models.Model):
 
 class TagAssets(models.Model):
     """资产标签表"""
+
     tag_name = models.CharField(max_length=32, unique=True, verbose_name='标签名')
     tag_c_day = models.DateField(auto_now_add=True, verbose_name='创建日期')
 
@@ -279,3 +289,283 @@ class TagAssets(models.Model):
         verbose_name_plural = verbose_name
 
 
+class IDCAssets(models.Model):
+    """机房资产表"""
+
+    idc_name = models.CharField('机房名称', max_length=64, unique=True)
+    idc_address = models.CharField('机房地址', max_length=100, unique=True)
+    idc_contract = models.ForeignKey('ContractAssets', null=True, blank=True, on_delete=models.SET_NULL,
+                                     verbose_name='机房合同')
+    idc_contact = models.CharField('机房联系人', max_length=32)
+    idc_telephone = models.CharField('支持电话', max_length=11, blank=True, null=True)
+    idc_memo = models.CharField('备注', max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return self.idc_name
+
+    class Meta:
+        db_table = 'imu_idc_assets'
+        verbose_name = '机房表'
+        verbose_name_plural = verbose_name
+
+
+class CabinetAssets(models.Model):
+    """机柜资产表"""
+
+    idc = models.ForeignKey('IDCAssets', related_name='cabinet', on_delete=models.CASCADE)
+    cabinet_name = models.CharField('机柜名称', max_length=64, unique=True)
+    cabinet_U_num = models.CharField('机柜总U数', max_length=100, blank=True)
+    cabinet_used = models.CharField('已使用空间', max_length=100, blank=True)
+    cabinet_memo = models.CharField('备注', max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return self.cabinet_name
+
+    class Meta:
+        db_table = 'imu_cabinet_assets'
+        verbose_name = '机柜表'
+        verbose_name_plural = verbose_name
+
+
+class ContractAssets(models.Model):
+    """合同资产表"""
+
+    contract_sn = models.CharField('合同号', max_length=128, unique=True)
+    contract_name = models.CharField('合同名称', max_length=100)
+    contract_price = models.IntegerField('合同金额')
+    contract_detail = models.TextField('详细合同', blank=True, null=True)
+    contract_start_day = models.DateField('生效日期', blank=True, null=True)
+    contract_end_day = models.DateField('失效日期', blank=True, null=True)
+    contract_c_day = models.DateField('创建时间', auto_now_add=True)
+    contract_m_day = models.DateField('修改日期', auto_now=True)
+    contract_memo = models.TextField('备注', blank=True, null=True)
+
+    def __str__(self):
+        return self.contract_name
+
+    class Meta:
+        db_table = 'imu_contract_assets'
+        verbose_name = "合同"
+        verbose_name_plural = verbose_name
+
+
+class NIC(models.Model):
+    """网卡组件"""
+
+    assets = models.ForeignKey('Assets', related_name='nic_assets', on_delete=models.CASCADE)  # 注意要用外键
+    nic_name = models.CharField('网卡名称', max_length=64, blank=True, null=True)
+    nic_model = models.CharField('网卡型号', max_length=128)
+    nic_mac = models.CharField('MAC地址', max_length=64)  # 虚拟机有可能会出现同样的mac地址
+    nic_ip_address = models.GenericIPAddressField('IP地址', blank=True, null=True)
+    nic_net_mask = models.CharField('掩码', max_length=64, blank=True, null=True)
+    nic_binding = models.CharField('绑定地址', max_length=64, blank=True, null=True)
+
+    def __str__(self):
+        return '%s:  %s:  %s' % (self.assets.asset_name, self.nic_model, self.nic_mac)
+
+    class Meta:
+        db_table = 'imu_nic_assets'
+        verbose_name = '网卡'
+        verbose_name_plural = verbose_name
+        unique_together = ('assets', 'nic_model', 'nic_mac')
+
+
+class DiskAssets(models.Model):
+    """硬盘设备"""
+
+    disk_interface_type_choice = (
+        ('SATA', 'SATA'),
+        ('SAS', 'SAS'),
+        ('SCSI', 'SCSI'),
+        ('SAS', 'SAS'),
+        ('IDE', 'IDE'),
+        ('Fiber Channel', 'Fiber Channel'),
+        ('UNKNOWN', 'UNKNOWN'),
+    )
+
+    assets = models.ForeignKey('Assets', related_name='disk_assets', on_delete=models.CASCADE)
+    disk_sn = models.CharField('硬盘SN号', max_length=128)
+    disk_slot = models.CharField('所在插槽位', max_length=64, blank=True, null=True)
+    disk_model = models.CharField('磁盘型号', max_length=128, blank=True, null=True)
+    disk_brand = models.CharField('磁盘制造商', max_length=128, blank=True, null=True)
+    disk_volume = models.FloatField('磁盘容量(GB)', blank=True, null=True)
+    disk_interface_type = models.CharField('接口类型', max_length=16, choices=disk_interface_type_choice,
+                                           default='unknown')
+
+    def __str__(self):
+        return '%s:  %s:  %s:  %sGB' % (self.assets.asset_name, self.disk_model, self.disk_slot, self.disk_volume)
+
+    class Meta:
+        db_table = 'imu_disk_assets'
+        verbose_name = '硬盘'
+        verbose_name_plural = verbose_name
+        unique_together = ('asset', 'sn')
+
+
+class RAMAssets(models.Model):
+    """内存组件"""
+
+    assets = models.ForeignKey('Assets', related_name='ram_assets', on_delete=models.CASCADE)
+    ram_sn = models.CharField('SN号', max_length=128, blank=True, null=True)
+    ram_model = models.CharField('内存型号', max_length=128, blank=True, null=True)
+    ram_brand = models.CharField('内存制造商', max_length=128, blank=True, null=True)
+    ram_slot = models.CharField('插槽', max_length=64)
+    ram_volume = models.IntegerField('内存大小(GB)', blank=True, null=True)
+
+    def __str__(self):
+        return '%s: %s: %s: %s' % (self.assets.asset_name, self.ram_model, self.ram_slot, self.ram_volume)
+
+    class Meta:
+        db_table = 'imu_ram_assets'
+        verbose_name = '内存'
+        verbose_name_plural = verbose_name
+        unique_together = ('assets', 'RAM_slot')
+
+
+class CPUAssets(models.Model):
+    """CPU组件"""
+
+    assets = models.OneToOneField('Assets', related_name='cpu_assets', on_delete=models.CASCADE)
+    cpu_model = models.CharField('CPU型号', max_length=128, blank=True, null=True)
+    cpu_count = models.PositiveSmallIntegerField('物理CPU个数', default=1)
+    cpu_core_count = models.PositiveSmallIntegerField('CPU核数', default=1)
+
+    def __str__(self):
+        return self.assets.asset_name + ":   " + self.cpu_model
+
+    class Meta:
+        db_table = 'imu_cpu_assets'
+        verbose_name = 'CPU'
+        verbose_name_plural = verbose_name
+
+
+class DomainAssets(models.Model):
+    """域名资产表"""
+
+    domain_name = models.CharField('域名名称', max_length=64, blank=True, null=True)
+    domain_address = models.URLField('域名地址', max_length=100, null=False)
+    domain_int_ip = models.GenericIPAddressField('对内IP地址', blank=True, null=True)
+    domain_out_ip = models.GenericIPAddressField('对外IP地址', blank=True, null=True)
+    domain_department = models.CharField('所属部门', blank=True, null=True)
+    domain_admin = models.ForeignKey('users.UserProfile', blank=True, related_name='domain_admin',
+                                     verbose_name='域名管理员', on_delete=models.PROTECT)
+
+
+class ProviderAssets(models.Model):
+    """供应商信息表"""
+
+    provider_name = models.CharField('供应商名称', max_length=64, unique=True)
+    provider_contact = models.CharField('技术支持人员', max_length=32, blank=True, null=True)
+    provider_phone = models.CharField('支持电话', max_length=11, blank=True, null=True)
+    provider_email = models.EmailField('供应商邮箱', unique=True)
+    provider_memo = models.CharField('备注', max_length=128, blank=True, null=True)
+
+    class Meta:
+        db_table = 'imu_provider_assets'
+        verbose_name = '供应商表'
+        verbose_name_plural = verbose_name
+
+
+class CloudAssets(models.Model):
+    """云平台资产表"""
+
+    cloud_type_choice = (
+        (0, '公有云'),
+        (1, '私有云'),
+    )
+    cloud_type = models.CharField('云类型', choices=cloud_type_choice, default=1)
+    cloud_brand = models.CharField('云厂商', max_length=100, blank=True, null=True)
+    cloud_admin = models.ForeignKey('users.UserProfile', blank=True, related_name='domain_admin',
+                                    verbose_name='云平台管理员', on_delete=models.PROTECT)
+
+
+class OtherAssets(models.Model):
+    """其他资产表"""
+
+    other_name = models.CharField('资产名称', max_length=64, unique=True)
+    other_sn = models.CharField('SN号', max_length=128, blank=True, null=True)
+    other_admin = models.ForeignKey('users.UserProfile', blank=True, related_name='domain_admin',
+                                    verbose_name='资产管理员', on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.other_name
+
+    class Meta:
+        db_table = 'imu_other_assets'
+        verbose_name = '其他资产表'
+        verbose_name_plural = verbose_name
+
+
+class EventLog(models.Model):
+    """日志，关联的资源被删除时相关的日志不能被删除，应当被保留"""
+
+    name = models.CharField('事件名称', max_length=128)
+    event_type_choice = (
+        (0, '其他'),
+        (1, '硬件变更'),
+        (2, '新增配件'),
+        (3, '设备下线'),
+        (4, '设备上线'),
+        (5, '定期维护'),
+        (6, '系统升级'),
+        (7, '业务上线/变更/更新'),
+    )
+
+    asset = models.ForeignKey('Assets', blank=True, null=True, on_delete=models.SET_NULL)
+    new_asset = models.ForeignKey('NewAssetApprovalZone', blank=True, null=True, on_delete=models.SET_NULL)
+    event_type = models.SmallIntegerField('事件类型', choices=event_type_choice, default=0)
+    component = models.CharField('事件子项', max_length=256, blank=True, null=True)
+    detail = models.TextField('事件详情')
+    date = models.DateTimeField('事件时间', auto_now_add=True)
+    user = models.ForeignKey('users.UserProfile', blank=True, null=True, verbose_name="事件执行人",
+                             on_delete=models.SET_NULL)
+    memo = models.TextField('备注', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'imu_log_assets'
+        verbose_name = "事件记录"
+        verbose_name_plural = verbose_name
+
+
+class NewAssetApprovalZone(models.Model):
+    """新资产待审批区域"""
+
+    sn = models.CharField('资产SN号', max_length=128, unique=True)  # 此字段必填
+    asset_type_choice = (
+        ('server', '服务器'),
+        ('network', '网络设备'),
+        ('storage', '存储设备'),
+        ('security', '安全设备'),
+        ('software', '软件资产'),
+        ('office', '办公设备'),
+        ('other', '其他设备'),
+    )
+    asset_type = models.CharField('资产类型', choices=asset_type_choice, default='server', max_length=64, blank=True,
+                                  null=True)
+    manufacturer = models.CharField('生产厂商', max_length=64, blank=True, null=True)
+    model = models.CharField('型号', max_length=128, blank=True, null=True)
+    ram_size = models.PositiveIntegerField('内存大小', blank=True, null=True)
+    cpu_model = models.CharField('CPU型号', max_length=128, blank=True, null=True)
+    cpu_count = models.PositiveSmallIntegerField('CPU物理数量', blank=True, null=True)
+    cpu_core_count = models.PositiveSmallIntegerField('CPU核心数量', blank=True, null=True)
+    os_distribution = models.CharField('发行商', max_length=64, blank=True, null=True)
+    os_type = models.CharField('系统类型', max_length=64, blank=True, null=True)
+    os_release = models.CharField('操作系统版本号', max_length=64, blank=True, null=True)
+
+    data = models.TextField('资产数据')  # 此字段必填
+
+    c_time = models.DateTimeField('汇报日期', auto_now_add=True)
+    m_time = models.DateTimeField('数据更新日期', auto_now=True)
+    approved = models.BooleanField('是否批准', default=False)
+
+    def __str__(self):
+        return self.sn
+
+    class Meta:
+        db_table = 'imu_NewAssetApprovalZone'
+        verbose_name = '新上线待批准资产'
+        verbose_name_plural = verbose_name
+        ordering = ['-c_time']
---
+++ test/bin/test_assets.py
@@ -0,0 +1,164 @@
+# coding=utf-8
+"""
+# @Time    : 10/14/19 4:37 PM
+# @Author  : F0rGeEk@root
+# @Email   : bat250@protonmail.com
+# @File    : test_assets.py
+# @Software: PyCharm
+***********************************************************
+███████╗ ██████╗ ██████╗  ██████╗ ███████╗███████╗██╗  ██╗
+██╔════╝██╔═████╗██╔══██╗██╔════╝ ██╔════╝██╔════╝██║ ██╔╝
+█████╗  ██║██╔██║██████╔╝██║  ███╗█████╗  █████╗  █████╔╝ 
+██╔══╝  ████╔╝██║██╔══██╗██║   ██║██╔══╝  ██╔══╝  ██╔═██╗ 
+██║     ╚██████╔╝██║  ██║╚██████╔╝███████╗███████╗██║  ██╗
+╚═╝      ╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚══════╝╚═╝  ╚═╝
+***********************************************************
+"""
+import json
+
+import urllib.request
+import urllib.parse
+
+import os
+import sys
+
+# BASE_DIR = os.path.dirname(os.getcwd())
+# # 设置工作目录，使得包和模块能够正常导入
+# sys.path.append(BASE_DIR)
+from test.conf import settings
+
+
+def update_test(data):
+    """
+    创建测试用例
+    :return:
+    """
+    # 将数据打包到一个字典内，并转换为json格式
+    data = {"asset_data": json.dumps(data)}
+    # 根据settings中的配置，构造url
+    url = "http://%s:%s%s" % (settings.Params['server'], settings.Params['port'], settings.Params['url'])
+    print('正在将数据发送至： [%s]  ......' % url)
+    try:
+        # 使用Python内置的urllib.request库，发送post请求。
+        # 需要先将数据进行封装，并转换成bytes类型
+        data_encode = urllib.parse.urlencode(data).encode()
+        response = urllib.request.urlopen(url=url, data=data_encode, timeout=settings.Params['request_timeout'])
+        print("\033[31;1m发送完毕！\033[0m ")
+        message = response.read().decode()
+        print("返回结果：%s" % message)
+    except Exception as e:
+        message = "发送失败"
+        print("\033[31;1m发送失败，%s\033[0m" % e)
+
+
+if __name__ == '__main__':
+    windows_data = {
+        "os_type": "Windows",
+        "os_release": "2018 64bit R2",
+        "os_distribution": "Microsoft",
+        "asset_type": "server",
+        "cpu_count": 8,
+        "cpu_model": "Intel(R) Core(TM) i7-9750H CPU @ 2.80GHz",
+        "cpu_core_count": 32,
+        "ram": [
+            {
+                "slot": "A1",
+                "capacity": 64,
+                "model": "Physical Memory",
+                "manufacturer": "kingstone ",
+                "sn": "123456"
+            },
+
+        ],
+        "manufacturer": "Dell inc.",
+        "model": "Y9320K 2019",
+        "wake_up_type": 3,
+        "sn": "31233-OEM-8992662-1321122",
+        "physical_disk_driver": [
+            {
+                "iface_type": "unknown",
+                "slot": 0,
+                "sn": "34567899234456276543234234566544",
+                "model": "SAMSUNG SV100S264G ATA Device",
+                "manufacturer": "(标准磁盘驱动器)",
+                "capacity": 1024
+            },
+            {
+                "iface_type": "SATA",
+                "slot": 1,
+                "sn": "3123345654323456787654334765423",
+                "model": "Seagate SV100S264G ATA Device",
+                "manufacturer": "(标准磁盘驱动器)",
+                "capacity": 1024
+            },
+
+        ],
+        "nic": [
+            {
+                "mac": "14:CF:22:EF:33:12",
+                "model": "[00000033] Realtek RTL8192CU Wireless LAN 802.11n USB 2.0 Network Adapter",
+                "name": 14,
+                "ip_address": "10.8.8.14",
+                "net_mask": [
+                    "255.255.255.0",
+                    "64"
+                ]
+            },
+            {
+                "mac": "0A:01:33:33:00:14",
+                "model": "[00000333] VmWare WorkStation Host-Only Ethernet Adapter",
+                "name": 24,
+                "ip_address": "192.168.56.14",
+                "net_mask": [
+                    "255.255.255.0",
+                    "64"
+                ]
+            },
+            {
+                "mac": "14:CF:22:FF:48:14",
+                "model": "Intel Adapter",
+                "name": 14,
+                "ip_address": "192.1.1.14",
+                "net_mask": ""
+            },
+
+
+        ]
+    }
+
+    linux_data = {
+        "asset_type": "server",
+        "manufacturer": "IBM.",
+        "sn": "F3LN110",
+        "model": "K1 Power S930",
+        "uuid": "4c4c4523-0039-4d20-804e-c6c2wwe31",
+        "wake_up_type": "Power Switch",
+        "os_distribution": "Ubuntu",
+        "os_release": "Ubuntu 16.04.4 LTS",
+        "os_type": "Linux",
+        "cpu_count": "8",
+        "cpu_core_count": "32",
+        "cpu_model": "POWER 9",
+        "ram": [
+            {
+                "slot": "A1",
+                "capacity": 64,
+                "model": "Physical Memory",
+                "manufacturer": "IBM",
+                "sn": "623466"
+            }
+        ],
+        "ram_size": 63.538997344970703,
+        "nic": [],
+        "physical_disk_driver": [
+            {
+                "model": "ST1033LM035-1RK015",
+                "size": "1024",
+                "sn": "WL109C15"
+            }
+        ]
+    }
+
+    update_test(linux_data)
+    update_test(windows_data)
+
